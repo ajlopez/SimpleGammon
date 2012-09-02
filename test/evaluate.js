3228821d2f20fd99741a52db4adaef1e4e67795d
ajlopez <webmaster@ajlopez.com>
2012-09-02 19:29:19 -0300

    evaluate with alldices parameter

--- test/evaluate.js
+++ test/evaluate.js
@@ -12,21 +12,23 @@ var board = new simplegammon.Board();
 board.putChecker(Colors.Red, 24);
 var game = new simplegammon.Game(board);
 
+var allonedice = [[1], [2], [3], [4], [5], [6]];
+
 // Evaluate level 0
 
-var result = game.evaluate(Colors.Red, 0);
+var result = game.evaluate(Colors.Red, 0, allonedice);
 
 assert.equal(result, 25);
 
 // Evaluate level 1
 
-var result = game.evaluate(Colors.Red, 1);
+var result = game.evaluate(Colors.Red, 1, allonedice);
 
 assert.equal(result, (24+23+22+21+20+19)/6);
 
 // Evaluate level 2
 
-var result = game.evaluate(Colors.Red, 2);
+var result = game.evaluate(Colors.Red, 2, allonedice);
 
 assert.equal(result, (24+23+22+21+20+19)/6);
 
@@ -36,13 +38,13 @@ board.putChecker(Colors.Black, 24);
 
 // Evaluate level 1
 
-result = game.evaluate(Colors.Red, 1);
+result = game.evaluate(Colors.Red, 1, allonedice);
 
 assert.equal(result, (24-25+23-25+22-25+21-25+20-25+19-25)/6);
 
 // Evaluate level 2
 
-result = game.evaluate(Colors.Red, 2);
+result = game.evaluate(Colors.Red, 2, allonedice);
 
 assert.equal(result, 0);
 
