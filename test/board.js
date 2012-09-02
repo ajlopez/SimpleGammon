2969994fb3a5640bd347b946d9f17ce869124aa3
ajlopez <webmaster@ajlopez.com>
2012-09-02 15:52:34 -0300

    get set position, with test

--- test/board.js
+++ test/board.js
@@ -2,14 +2,25 @@
 var simplegammon = require('../'),
     assert = require('assert');
 
+var Colors = simplegammon.Colors;
+    
 assert.ok(simplegammon.Board);
 
 var board = new simplegammon.Board();
 
 assert.ok(board);
 
-assert.equal(board.getReds(), 0);
-assert.equal(board.getBlacks(), 0);
+assert.equal(board.countReds(), 0);
+assert.equal(board.countBlacks(), 0);
 
+for (var k=0; k <= 25; k++) {
+    assert.equal(board.getPosition(Colors.Red, k), 0);
+    assert.equal(board.getPosition(Colors.Black, k), 0);
+}
 
+board.putChecker(Colors.Red, 1);
+
+assert.equal(board.countReds(), 1);
+assert.equal(board.getPosition(Colors.Red, 1), 1);
+assert.equal(board.countBlacks(), 0);
 
