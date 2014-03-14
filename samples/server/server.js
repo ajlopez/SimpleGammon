5219b21e6bab10a8a78910a68db971354df8e6a6
ajlopez <webmaster@ajlopez.com>
2014-03-14 08:24:07 -0300

    Backgammon server, WIP

--- samples/server/server.js
+++ samples/server/server.js
@@ -7,6 +7,10 @@ var express = require('express')
 
 server.listen(8080);
 
+var Colors = simplegammon.Colors;
+var Board = simplegammon.Board;
+var Game = simplegammon.Game;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 var stopped = false;
@@ -14,6 +18,8 @@ var stopped = false;
 io.sockets.on('connection', function (socket) {
   socket.on('newplay', function (data) {
     console.dir(data);
+    var board = new Board(data.checkers[Colors.Red], data.checkers[Colors.Black]);
+    var game = new Game(board); 
   });
   
   socket.on('stop', function() {
